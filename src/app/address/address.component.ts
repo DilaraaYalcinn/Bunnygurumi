865bafeb243c281a07d91854e0ab62599bdb87fa
DilaraaYalcinn <dilaraayalcinn@gmail.com>
2021-03-16 15:57:47 +0300

    Address Validation
    Error messages and routing condition (valid or not)

--- src/app/address/address.component.ts
+++ src/app/address/address.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { ShippingAddressService } from '../shared/shipping-address.service';
 
 @Component({
@@ -10,17 +12,24 @@ import { ShippingAddressService } from '../shared/shipping-address.service';
 export class AddressComponent implements OnInit {
 
   public adress: string = '';
-  constructor( public service: ShippingAddressService) { }
+  constructor(public service: ShippingAddressService, private toastr: ToastrService, public router: Router) { }
 
   ngOnInit(): void {
   }
   onClick(form: NgForm) {
-    this.service.postAddress(form.value).subscribe(
-      result => {
-        console.log('success: ', result);
-      },
-      error => console.log('error: ', error)
-    );
+    if (form.valid) {
+      this.service.postAddress(form.value).subscribe(
+        result => {
+          console.log('success: ', result);
+          this.router.navigate(['payment']);
+        },
+        error => console.log('error: ', error)
+      );
+    } else{
+      console.log("Boş alanlar var");
+      this.toastr.error("Please fill the required fields", "*");
+    }
+    
     // console.log("form ",form)
     // var inputValue = (<HTMLInputElement>document.getElementById(address)).value;
   }
